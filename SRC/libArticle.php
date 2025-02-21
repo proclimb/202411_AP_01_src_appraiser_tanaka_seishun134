ad6735e1e03a4110e69c5e12e6fe4427c691a6be
seishun <202411ap01_tanaka@x-ide.sakura.ne.jp>
2025-02-21 14:21:20 +0900

    物件名のリンクを押してもDBに登録された値が表示されない

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -124,7 +124,11 @@ function subArticle()
 				$sellCharge  = $row["SELLCHARGE"];
 			?>
 				<tr>
-					<td class="list_td<?php print $i ?>"><a href="javascript:form.act.value='articleEdit';form.articleNo.value='<?php print $rrticleNo ?>';form.submit();"><?php print $article ?></a></td>
+
+					<td class="list_td<?php print $i ?>"><a href="javascript:form.act.value='articleEdit';form.articleNo.value=<?php print $articleNo ?>;form.submit();"><?php print $article ?></a></td>
+
+
+
 					<td class="list_td<?php print $i ?>"><?php print $room ?></td>
 					<td class="list_td<?php print $i ?>"><?php print $drawing ?></td>
 					<td class="list_td<?php print $i ?>"><?php print $keyPlace ?></td>
