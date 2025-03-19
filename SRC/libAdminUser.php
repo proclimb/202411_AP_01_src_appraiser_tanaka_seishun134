919ac2a52e6c13ee231103c4f64748ffd28cc873
seishun <202411ap01_tanaka@x-ide.sakura.ne.jp>
2025-03-19 15:57:27 +0900

    削除する時の確認メッセージが表示されない

--- SRC/libAdminUser.php
+++ SRC/libAdminUser.php
@@ -8,7 +8,11 @@ function subAdminUser()
 
 	subMenu();
 ?>
+	<script type="text/javascript" src="./js/adminUser.js"></script>
 	<h1>ユーザー情報画面</h1>
+
+
+
 
 	<form name="form" id="form" action="index.php" method="post">
 		<input type="hidden" name="act" value="adminUser" />
